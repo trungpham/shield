df4f7ec4121bff3f092df8569c0586670984c333
Mustafa TURAN <mustafaturan.net@gmail.com>
2016-08-12 00:13:57 +0100

    Updated  authable version.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Shield.Mixfile do
      {:phoenix_ecto, "~> 3.0"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:authable, "~> 0.6.3"},
+     {:authable, "~> 0.6.4"},
      {:shield_notifier, "~> 0.1.0"},
      {:cors_plug, git: "https://github.com/mustafaturan/cors_plug.git"},
      {:ex_machina, "~> 1.0.1", only: :test},
